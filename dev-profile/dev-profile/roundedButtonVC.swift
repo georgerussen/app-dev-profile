4e258786f8968a6d836a88cfa15a3f88f6fcb33c
George <Russen@Georges-MacBook-Air.local>
2018-01-17 19:26:00 +0000

    Built iPhone interface

---
+++ dev-profile/dev-profile/roundedButtonVC.swift
@@ -0,0 +1,19 @@
+//
+//  roundedButtonVC.swift
+//  dev-profile
+//
+//  Created by George on 17/01/2018.
+//  Copyright © 2018 George. All rights reserved.
+//
+
+import UIKit
+
+class imageRadius: UIImageView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.cornerRadius = 2.0
+    }
+
+}
